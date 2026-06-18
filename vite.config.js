5b1cfdf3816c8f90c4b9fa6714c2cca65b0463eb
Joslim2k25 <limjoe2021@gmail.com>
2026-06-18 12:55:34 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+javascriptimport { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
+
+I-check na walang extra text bago ang import sa line 1
+I-click "Commit changes"
+
+Sabihin mo kapag tapos na.You've used 90% of your session limitUpgrade
