1f4fd4fb9bc6253c2c3214ac448d828539649863
Joslim2k25 <limjoe2021@gmail.com>
2026-06-20 18:34:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,3 @@ export default defineConfig({
   plugins: [react()],
 })
 
-I-check na walang extra text bago ang import sa line 1
-I-click "Commit changes"
-
-Sabihin mo kapag tapos na.You've used 90% of your session limitUpgrade
